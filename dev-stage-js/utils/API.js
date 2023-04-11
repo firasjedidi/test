32f8e6bbb9d73570f22f7bdbfb0b82d89b3faf0b
firasjedidi <firasjedidi.com>
2023-04-11 16:15:40 +0100

    test

--- dev-stage-js/utils/API.js
+++
@@ -1,6 +0,0 @@
-import axios from "axios";
-
-export default axios.create({
-    baseURL: "https://admin.stage-tunisie.tn/api/",
-    responseType: "json"
-});
--- dev-stage-js/utils/functions.js
+++
@@ -1,5 +0,0 @@
-exports.htmlToReact = (htmlText) => {
-    const HtmlToReactParser = require('html-to-react').Parser
-    var htmlToReactParser = new HtmlToReactParser();
-    return htmlToReactParser.parse(htmlText);
-}
